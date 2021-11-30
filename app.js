a70796735c78db3025ce2c318b039d5f69fd96f5
Ben Beissner <bebe@entiac.com>
2021-11-30 12:43:30 +0100

    Jetzt mit Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! - Test-Durchlauf'));
+app.get('/', (req, res) => res.send('Hello World! - jetzt mit Pipeline von Ben'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
